7cc3e2ec1fb3f2082a6880a1c6c0b3f08bff1d5a
Marija Petrovic <marijap@protonmail.com>
2023-01-12 12:36:20 +0100

    improve code with Queue class

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from mpi4py import MPI
+from queue import Queue
 
 
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    queue1 = []
-    queue2 = []
     num_of_messages = 10
+    queue1 = Queue(maxsize=5)
+    queue2 = Queue(maxsize=5)
 
     # producer
     if rank == 0:
@@ -24,11 +25,10 @@ def main():
         for i in range(num_of_messages):
             try:
                 message = comm.recv(source=0, tag=11)
+                print("MESSAGE:", message)
                 if message['routing_key'] == 'queue1':
-                    # queue1.append(message)
                     comm.send(message, dest=2, tag=11)
                 else:
-                    # queue2.append(message)
                     comm.send(message, dest=3, tag=11)
             except MPI.Exception as e:
                 print("Exchange failed to receive or send message to consumer(s):", e.error_string)
@@ -37,17 +37,29 @@ def main():
         for i in range(num_of_messages // 2):
             try:
                 received_message = comm.recv(source=1, tag=11)
-                queue1.append(received_message)
+                queue1.put_nowait(received_message)
                 print("Consumer 1 received message:", received_message)
+                print("size1:", queue1.qsize())
+                while not queue1.empty():
+                    print("received 1")
+                    print("[1]:", queue1.get_nowait())
+                    if queue1.empty():
+                        break
             except MPI.Exception as e:
                 print("Consumer 1 failed to receive message from exchange:", e.error_string)
     # consumer 2
     elif rank == 3:
         for i in range(num_of_messages // 2):
             try:
-                received_message = comm.recv(source=1, tag=12)
-                queue2.append(received_message)
+                received_message = comm.recv(source=1, tag=11)
+                queue2.put_nowait(received_message)
                 print("Consumer 2 received message:", received_message)
+                print("size2:", queue2.qsize())
+                while not queue2.empty():
+                    print("received 2")
+                    print("[2]:", queue2.get_nowait())
+                    if queue2.empty():
+                        break
             except MPI.Exception as e:
                 print("Consumer 2 failed to receive message from exchange:", e.error_string)
 
