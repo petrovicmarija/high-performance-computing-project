4278835169e176a69da13da542118ae2b7279abd
Marija Petrovic <marijap@protonmail.com>
2023-01-17 23:27:55 +0100

    save messages to json

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import json
 
 
-def main():
+def run_initial():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
@@ -66,39 +66,24 @@ def main():
                 print("Consumer 2 failed to receive message from exchange:", e.error_string)
 
 
-def main_user_input():
+def run_with_user_input():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
     queue1 = Queue(maxsize=5)
     queue2 = Queue(maxsize=5)
 
-    # q1 = []
-    # q2 = []
-    # with open('consumer1.json') as json_file:
-    #     data = json.load(json_file)
-    #     q1 = data['queue1']
-    #
-    # with open('consumer2.json') as json_file:
-    #     data = json.load(json_file)
-    #     q2 = data['queue2']
-    #
-    # for msg in q1:
-    #     queue1.put(msg)
-    #
-    # for msg in q2:
-    #     queue2.put(msg)
     content = None
     routing_key = None
     # producer
     comm.Barrier()
     if rank == 0:
-        for i in range(3):
+        for i in range(5):
             try:
                 content = input("Enter message content: ")
                 if content == "stop":
                     sys.exit("Producer stopped working.")
-                routing_key = input("Enter routing key (options: queue1, queue2): ")
+                routing_key = input("Enter routing key (options: q1, q2): ")
                 message = {'message': content, 'routing_key': routing_key}
                 comm.send(message, dest=1, tag=11)
                 print("Producer sent message:", message)
@@ -110,10 +95,9 @@ def main_user_input():
         while True:
             try:
                 message = comm.recv(source=0, tag=11)
-                # print("MESSAGE:", message)
-                if message['routing_key'] == 'queue1':
+                if message['routing_key'] == 'q1':
                     comm.send(message, dest=2, tag=11)
-                elif message['routing_key'] == 'queue2':
+                elif message['routing_key'] == 'q2':
                     comm.send(message, dest=3, tag=11)
             except MPI.Exception as e:
                 print("Exchange failed to receive or send message to consumer(s):", e.error_string)
@@ -124,9 +108,7 @@ def main_user_input():
                 received_message = comm.recv(source=1, tag=11)
                 queue1.put_nowait(received_message)
                 print("Consumer 1 received message:", received_message)
-                # print("size1:", queue1.qsize())
                 while not queue1.empty():
-                    # print("received 1")
                     print("[1]:", queue1.get_nowait())
                     if queue1.empty():
                         break
@@ -139,9 +121,7 @@ def main_user_input():
                 received_message = comm.recv(source=1, tag=11)
                 queue2.put_nowait(received_message)
                 print("Consumer 2 received message:", received_message)
-                # print("size2:", queue2.qsize())
                 while not queue2.empty():
-                    # print("received 2")
                     print("[2]:", queue2.get_nowait())
                     if queue2.empty():
                         break
@@ -149,42 +129,85 @@ def main_user_input():
                 print("Consumer 2 failed to receive message from exchange:", e.error_string)
 
 
+def read_json(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def save_to_json_for_consumer(filename, data):
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+def run_with_files():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    queue1 = Queue(maxsize=5)
+    queue2 = Queue(maxsize=5)
+
+    content = None
+    routing_key = None
+    # producer
+    comm.Barrier()
+    if rank == 0:
+        for i in range(3):
+            try:
+                content = input("Enter message content: ")
+                if content == "stop":
+                    sys.exit("Producer stopped working.")
+                routing_key = input("Enter routing key (options: q1, q2): ")
+                message = {'message': content, 'routing_key': routing_key}
+                comm.send(message, dest=1, tag=11)
+                print("Producer sent message:", message)
+            except MPI.Exception as e:
+                print("Producer failed to send message to exchange:", e.error_string)
+    comm.Barrier()
+    # direct exchange
+    if rank == 1:
+        while True:
+            try:
+                message = comm.recv(source=0, tag=11)
+                if message['routing_key'] == 'q1':
+                    comm.send(message, dest=2, tag=11)
+                elif message['routing_key'] == 'q2':
+                    comm.send(message, dest=3, tag=11)
+            except MPI.Exception as e:
+                print("Exchange failed to receive or send message to consumer(s):", e.error_string)
+    # consumer 1
+    elif rank == 2:
+        while True:
+            try:
+                received_message = comm.recv(source=1, tag=11)
+                queue1.put_nowait(received_message)
+                print("Consumer 1 received message:", received_message)
+                while not queue1.empty():
+                    data = read_json("consumer1.json")
+                    data.append(queue1.get_nowait())
+                    save_to_json_for_consumer("consumer1.json", data)
+                    if queue1.empty():
+                        break
+            except MPI.Exception as e:
+                print("Consumer 1 failed to receive message from exchange:", e.error_string)
+    # consumer 2
+    elif rank == 3:
+        while True:
+            try:
+                received_message = comm.recv(source=1, tag=11)
+                queue2.put_nowait(received_message)
+                print("Consumer 2 received message:", received_message)
+                while not queue2.empty():
+                    data = read_json("consumer2.json")
+                    data.append(queue2.get_nowait())
+                    save_to_json_for_consumer("consumer2.json", data)
+                    if queue2.empty():
+                        break
+            except MPI.Exception as e:
+                print("Consumer 2 failed to receive message from exchange:", e.error_string)
+
+
 if __name__ == '__main__':
-    # main()
-    # file_c1 = open('consumer1.json')
-    # data = json.load(file_c1)
-    # for i in data['queue1']:
-    #     print(i.content)
-    #
-    # file_c1.close()
-
-    # consumer1_messages = []
-    # with open('consumer1.json') as file:
-    #     for message in file:
-    #         message_dict = json.loads(message)
-    #         consumer1_messages.append(message_dict)
-    #
-    # for msg in consumer1_messages:
-    #     print(msg["content"])
-
-
-    # file = open('consumer2.json', "r")
-    # data = json.loads(file.read())
-    # for i in data['queue2']:
-    #     print(i)
-    #
-    # file.close()
-
-    # q2 = []
-    # with open('consumer2.json') as json_file:
-    #     data = json.load(json_file)
-    #
-    #     print(data['queue2'])
-    #     print(type(data['queue2']))
-    #     q2 = data['queue2']
-    #
-    # for i in range(len(q2)):
-    #     print(q2[i].get("content"))
-    #     print(type(q2[i]))
-
-    main_user_input()
+    # run_initial()
+    # run_with_user_input()
+    run_with_files()
